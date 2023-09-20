ae3548e79239aa08bf0e2769f5f54f8d4ab1489a
Guillermo Orio Fernández <a22guillermoof@lnxd-i1e07.ateis.local>
2023-09-20 13:14:12 +0200

    Modificado: conf de node completo y testeado

--- configuracionnode.bash
+++ configuracionnode.bash
@@ -4,4 +4,6 @@ sudo apt install -y git
 sudo apt install -y curl
 sudo curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh
 bash install_nvm.sh
-gnome-terminal -- bash -c "nvm install --lts"
+gnome-terminal -- bash -ic "nvm install --lts"
+gnome-terminal -- bash -ic "node"
+echo "fin"
